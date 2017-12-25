cbc9bc3597560a12b9f6f983a5fe4a14062dc251
MiguelFS86 <miguel.fuentes.sobrin@gmail.com>
2017-12-25 12:37:51 +0100

    <feature> Test

--- src/app/services/lista-deseos.service.ts
+++ src/app/services/lista-deseos.service.ts
@@ -11,10 +11,12 @@ export class ListaDeseosService {
 		let lista1 = new Lista('Compras de supermercado');
 		let lista2 = new Lista('Juegos que deseo');
 		let lista3 = new Lista('Cosas de la universidad');
+		let lista4 = new Lista('Otro elemento mas');
 
 		this.listas.push(lista1);
 		this.listas.push(lista2);
 		this.listas.push(lista3);
+		this.listas.push(lista4);
 
 		console.log("Servicio inicializado");
 	}
